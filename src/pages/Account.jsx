0639aed2e7eace9dbc4dec25cc9baa329eeeaade
Alyosha Baryshnikov <alohahoy@gmail.com>
2024-10-18 20:11:31 +0100

    mini changes

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,10 +1,45 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../contexts/UserContext";
 
 
 const Account = () => {
 
     const { user } = useContext(UserContext)
+    const [userToken, setUserToken] = useState('')
+
+    const generateUserToken = () => {
+        
+        // take in user uid
+
+        // transform using encryption algorithm with a version number
+        // call backend so the process is secure
+        fetch('/api/generate', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                uid: user.uid
+            })
+        })
+        .then((res) => {
+            if (res.ok) {
+                return res.json()
+            }
+        }).then( (data) => {
+            setUserToken(data.token)
+        }).catch( (err) => {
+            console.error(err)
+        })
+
+        // save to db?
+
+
+        // setUserToken(getRandomString())
+    }
+
+
+    const copyToken = (token) => {
+        navigator.clipboard.writeText(token)
+    }
 
     return ( 
 
@@ -17,6 +52,20 @@ const Account = () => {
                 <li className="text-black">Reset Password</li>
                 <li className="text-black">Delete Account</li>
             </ul>
+
+
+            <div className="bg-gray-200 p-6">
+                <p className="text-black p-2 text-lg"> Generate a user token and an api token for your api. </p>
+                <div className="text-black">
+                    <button onClick={generateUserToken} className="bg-gray-200 p-2"> Generate User Token </button>
+                    <div className="flex justify-between">
+                        <p className="text-white bg-black p-4 w-4/5 break-words whitespace-normal"> { userToken }</p>
+                        <button onClick={() => {copyToken(userToken)}} className="bg-blue-500 text-white p-4"> Copy </button>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
 
     );
